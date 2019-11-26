5a2b784fec543c777b3607bf85abd8d36a7e12d8
frieupet <florian.rieupet@atinternet.com>
2019-11-26 10:31:01 +0100

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -2,35 +2,43 @@ ATInternet.Scroll = ATInternet.Scroll || {};
 ATInternet.Tracker.Plugins.Scroll = function(parent) {
 	var _this = this,
 		maxScroll = 0,
-        documentHeight = $(document).height(),
-        defaultfirst = 30 / 100 * documentHeight,
-        defaultinterval = 10 / 100 * documentHeight,
+		documentHeight = $(document).height(),
+		defaultfirst = 40 / 100 * documentHeight,
+		defaultinterval = 20 / 100 * documentHeight,
 		first,
 		interval;
-        
+	
 	parent.Scroll = {};
 	parent.Scroll.init = function(params) {
-			
+
 		if(params) {
+            // ---> UPDATE: please not that the updated script was not tested with params
 			first = parseInt((params.hasOwnProperty('first')) ? params.first : defaultfirst);
 			interval = parseInt((params.hasOwnProperty('interval')) ? params.interval : defaultinterval);
-			
+
 			$(document).scroll(function(e) {
-				// grab the scroll amount and the window height
-				var scrollAmount = $(window).scrollTop();
+                // grab the scroll amount and the window height
+                // ---> UPDATE: add window height to scollAmount to trigger when threshold comes into side not gets out of sight
+				var scrollAmount = $(window).height() + $(window).scrollTop();
 				if (scrollAmount < maxScroll) {
 					return
 				}
                 
-				maxScroll = parseInt(scrollAmount + interval);
+                // UPDATE: relocated 'maxScroll'
 				//       scrollAT = '[' + maxScroll + ']';
 				// calculate the percentage the user has scrolled down the page
 				var scrollPercent = (scrollAmount / documentHeight) * 100;
-				var scrollAT = '[' + Math.round(parseInt(scrollPercent / 10) * 10) + '%25]';
+				// ---> UPDATE: relocated 'scrollAT'
 				if (scrollAmount > first) {
+                    // ---> UPDATE: increment maxScroll once with first and then each hit by interval
+					maxScroll = maxScroll + first + interval;
+                    // ---> UPDATE: calculation of % via maxScroll for values equal to interval steps
+                    var scrollAT = '[' + (Math.floor(maxScroll/documentHeight * 10) * 10) + '%25]';
 					sendATHit();
+                    // ---> UPDATE: reset first to 0 so it will be added to maxScroll only once
+                    first = 0;
 				}
-                
+
 
 				function sendATHit() {
 					parent.publisher.send({
